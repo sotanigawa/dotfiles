a1aa5731b20809687843b7a99abc3176d63a4efc
SotaroTanigawa <sotanigawa@gmail.com>
2021-03-09 04:38:35 +0900

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DOT_FILES=(
+    .emacs.d/init.el
+    .gitconfig
+    .zshrc
+)
+
+for file in ${DOT_FILES[@]}
+do
+    src=~/dotfiles/$file
+    dest=~/$file
+    if [ "`readlink $dest`" = $src ]; then
+        echo -n '[ok] '
+    else
+        if [ ! $1 = 'test' ]; then
+            mkdir -pv `dirname $dest`
+            ln -snfi $src $dest
+        fi
+        echo -n '[ln] '
+    fi
+    echo "$dest -> $src"
+done
