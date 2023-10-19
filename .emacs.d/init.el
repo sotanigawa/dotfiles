146756c156bc5973c3909b705e0774da729c83e1
SotaroTanigawa <sotanigawa@gmail.com>
2023-10-19 12:17:46 +0900

    .emacs.d/init.el: use custom-set-* functions

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,43 +1,47 @@
 ;;; Variables
-(setq inhibit-startup-message t)
-(setq confirm-kill-emacs 'y-or-n-p)
-(setq use-dialog-box nil)
-(setq load-prefer-newer t)
-(setq vc-follow-symlinks t)
-(setq mouse-wheel-scroll-amount '(2 ((shift) . 4) ((control))))
-(setq mouse-wheel-progressive-speed nil)
-(setq scroll-preserve-screen-position 'always)
-(setq-default show-trailing-whitespace t)
-(setq-default indent-tabs-mode nil)
-(setq recentf-save-file (locate-user-emacs-file "share/.recentf"))
-(setq recentf-max-saved-items 1000)
-(setq backup-directory-alist
-      `((".*" . ,(locate-user-emacs-file "temp/backups/"))))
-(setq auto-save-file-name-transforms
-      `((".*" ,(locate-user-emacs-file "temp/auto-saves/") t)))
-(setq auto-save-list-file-prefix
-      (locate-user-emacs-file "temp/auto-saves/.saves-"))
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(when (file-exists-p custom-file) (load custom-file))
-(setq desktop-path (list (locate-user-emacs-file "share/")))
+(custom-set-variables
+ ;; Entering and Exiting
+ '(inhibit-startup-screen t)
+ '(confirm-kill-emacs 'y-or-n-p)
+ ;; Controlling the Display
+ '(scroll-preserve-screen-position 'always)
+ '(show-trailing-whitespace t)
+ '(line-number-mode t)
+ '(column-number-mode t)
+ '(global-hl-line-mode t)
+ '(global-display-line-numbers-mode t)
+ ;; Frames and Graphical Displays
+ '(mouse-wheel-scroll-amount '(2 ((shift) . 4) ((control))))
+ '(mouse-wheel-progressive-speed nil)
+ '(scroll-bar-mode nil)
+ '(menu-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(use-dialog-box nil)
+ ;; Backup and Auto-Save Files
+ '(backup-directory-alist
+   `((".*" . ,(locate-user-emacs-file "temp/backups/"))))
+ '(auto-save-file-name-transforms
+   `((".*" ,(locate-user-emacs-file "temp/auto-saves/") t)))
+ '(auto-save-list-file-prefix
+   (locate-user-emacs-file "temp/auto-saves/.saves-"))
+ ;; Editing
+ '(indent-tabs-mode nil)
+ '(show-paren-mode t)
+ ;; Miscellaneous
+ '(load-prefer-newer t)
+ '(vc-follow-symlinks t)
+ '(fido-vertical-mode t)
+ '(recentf-save-file (locate-user-emacs-file "share/.recentf"))
+ '(recentf-max-saved-items 1000)
+ '(custom-file (locate-user-emacs-file "custom.el")))
 
-;;; Modes
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(line-number-mode 1)
-(column-number-mode 1)
-(global-display-line-numbers-mode 1)
-(global-hl-line-mode 1)
-(show-paren-mode 1)
-(when (version<= "28" emacs-version) (fido-vertical-mode 1))
+;;; Faces
+(custom-set-faces
+ '(default ((t (:family "RictyDiscord" :height 140)))))
 
 ;;; Keymaps
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "C-x C-r") 'recentf)
-
-;;; Font
-(add-to-list 'default-frame-alist '(font . "RictyDiscord-14"))
 
 ;;; Package System
 (require 'package)
