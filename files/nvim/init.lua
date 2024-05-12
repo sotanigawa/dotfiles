386db87349d0b86a25335f6a093a2aa68ca9da8a
SotaroTanigawa <sotanigawa@gmail.com>
2024-05-13 00:05:07 +0900

    nvim: use lazy.nvim for plugin management

--- files/nvim/init.lua
+++ files/nvim/init.lua
@@ -1,16 +1,38 @@
-local Plug = vim.fn['plug#']
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
 
-vim.call('plug#begin')
-Plug('nvim-lualine/lualine.nvim')
-Plug('ntpeters/vim-better-whitespace')
-Plug('Mofiqul/dracula.nvim')
-vim.call('plug#end')
-
-require('lualine').setup {
-    options = {
-        icons_enabled = false,
-    }
-}
+require("lazy").setup({
+    {
+        "Mofiqul/dracula.nvim",
+        config = function()
+            vim.cmd([[colorscheme dracula]])
+        end,
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        config = function()
+            require('lualine').setup {
+                options = {
+                    icons_enabled = false,
+                    theme = 'dracula-nvim',
+                }
+            }
+        end,
+    },
+    {
+        "ntpeters/vim-better-whitespace",
+    },
+})
 
 vim.opt.encoding = "utf-8"
 vim.opt.number = true
@@ -23,5 +45,3 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
-
-vim.cmd[[colorscheme dracula]]
