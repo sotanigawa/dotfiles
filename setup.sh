4f2e20fd71c54ffc24771fcfe5641e09a3453fa1
SotaroTanigawa <sotanigawa@gmail.com>
2026-05-11 03:53:34 +0900

    refactor: use source+override for ranger config instead of copy+edit

--- setup.sh
+++ setup.sh
@@ -25,30 +25,16 @@ create_symlink() {
     run ln -snf "$src" "$dst"
 }
 
-ranger_rc_conf_setup() {
-    local rc_conf_default="/usr/share/doc/ranger/examples/rc_emacs.conf"
-    local rc_conf="$HOME/.config/ranger/rc.conf"
-    if [[ -e "$rc_conf" ]]; then
-        echo "[already exists] $rc_conf"
-        return
-    fi
-    echo "[copy] $rc_conf_default -> $rc_conf"
-    run mkdir -pv "$(dirname "$rc_conf")"
-    run cp "$rc_conf_default" "$rc_conf"
-    run sed -i "s/set mouse_enabled true/set mouse_enabled false/" "$rc_conf"
-}
-
 files="$(cd "$(dirname "$0")"; pwd)/files"
 
 create_symlink "$files/alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
 create_symlink "$files/emacs/init.el"            "$HOME/.config/emacs/init.el"
 create_symlink "$files/git/config"               "$HOME/.config/git/config"
 create_symlink "$files/nvim/init.lua"            "$HOME/.config/nvim/init.lua"
+create_symlink "$files/ranger/rc.conf"           "$HOME/.config/ranger/rc.conf"
 create_symlink "$files/ranger/rifle.conf"        "$HOME/.config/ranger/rifle.conf"
 create_symlink "$files/stumpwm/config"           "$HOME/.config/stumpwm/config"
 create_symlink "$files/X11/xinitrc"              "$HOME/.config/X11/xinitrc"
 create_symlink "$files/X11/Xresources"           "$HOME/.config/X11/Xresources"
 create_symlink "$files/zsh/zshenv"               "$HOME/.zshenv"
 create_symlink "$files/zsh/zshrc"                "$HOME/.config/zsh/.zshrc"
-
-ranger_rc_conf_setup
